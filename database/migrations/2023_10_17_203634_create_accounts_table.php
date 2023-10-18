7248ca39b2a5108dc66e9f502bd9c2487b721f5d
Bruno Costa <bruno.costa@dafiti.com.br>
2023-10-18 10:27:07 -0300

    wip

--- database/migrations/2023_10_17_203634_create_accounts_table.php
+++ database/migrations/2023_10_17_203634_create_accounts_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -14,12 +13,12 @@ return new class extends Migration
         Schema::create('accounts', function (Blueprint $table) {
             $table->uuid('id')->primary();
             $table->uuid('bank');
-            $table->uuid('agency');
             $table->string('name');
+            $table->uuid('agency');
             $table->string('number');
             $table->timestamps();
 
-            $table->unique(['agency', 'number']);
+            $table->unique(['bank', 'agency', 'number']);
         });
     }
 
